d1679d7a1137369518b53d0317de628748966d4f
solomon-2105 <solomon.prem.sinai@gmail.com>
2026-02-25 18:10:10 +0530

    added

---
+++ index.js
@@ -0,0 +1,17 @@
+var r1 = Math.floor(Math.random()*6)+1;
+r1_image = "images/dice"+r1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",r1_image);
+
+var r2 = Math.floor(Math.random()*6)+1;
+r2_image = "images/dice"+r2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",r2_image);
+
+if (r1>r2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (r2>r1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
